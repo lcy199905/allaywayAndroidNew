6f2edc03a5214ef3d91671a684edebcd9f5c81b2
cyh <cyh@qq.com>
2019-06-12 19:49:04 +0800

    cyh

--- app/src/main/java/com/alleyway/activitys/HomeActivity.java
+++ app/src/main/java/com/alleyway/activitys/HomeActivity.java
@@ -9,18 +9,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.alleyway.R;
 
 public class HomeActivity extends Fragment {
 
+    private ListView work_list_item;
+
     public HomeActivity(){
 
     }
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        work_list_item=getActivity().findViewById(R.id.work_list_item);
+
         return inflater.inflate(R.layout.activity_home, container ,false);
+
     }
+
+
+
 }
